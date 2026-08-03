24bff8425e0166ae9ab3a34fe896fc9a2e8fd90b
ZM0009 <zmcewen@gmail.com>
2026-08-03 17:47:58 +0800

    Delete service-worker.js

--- service-worker.js
+++
@@ -1,5 +0,0 @@
-const CACHE='sethours-v1.1.0-ios-icons';
-const ASSETS=['./','./index.html','./styles.css','./app.js','./manifest.json','./apple-touch-icon.png','./favicon-32.png','./favicon-16.png','./icons/icon-120.png','./icons/icon-152.png','./icons/icon-167.png','./icons/icon-180.png','./icons/icon-192.png','./icons/icon-384.png','./icons/icon-512.png'];
-self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));self.skipWaiting()});
-self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));self.clients.claim()});
-self.addEventListener('fetch',e=>{if(e.request.method!=='GET')return;e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).then(resp=>{const copy=resp.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));return resp}).catch(()=>caches.match('./index.html'))))});
